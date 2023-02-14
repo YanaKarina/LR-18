d790b3dfd72754cdbe286a09421e482d3ce2f5bc
YanaKarina <117731339+YanaKarina@users.noreply.github.com>
2023-02-14 17:36:37 +0300

    Add files via upload

---
+++ Задание по ЛР 18.pas
@@ -0,0 +1,35 @@
+﻿Program zadanie1;
+Var
+  f,g: File of Integer;
+  s: String;
+  i,x: Integer;
+  a: Array [1..30] of Integer;
+begin
+Writeln('Введите полное имя файла: ')
+Readln(s);
+Assign(f,s);
+Reset(f);
+Writeln('Файл F:');
+While not Eof(f) do
+begin
+  Read(f,x);
+  Write(x,' ');
+  i:=i+1;
+  a[i]:=x;
+end;
+Writeln;
+Assign(g,'4.dat');
+Rewrite(g);
+For i:=30 downto 1 do
+Write(g,a[i]);
+Reset(g);
+Writeln('Файл G:');
+While not Eof(g) do
+  begin
+  Read(g,x);
+  Write(x,' ');
+end;
+Close(f);
+Close(g);
+Readln
+end.
